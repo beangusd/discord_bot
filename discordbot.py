424cbe28aa2ef6a8da091843bc4452113f821bb6
Roger Yi <80113181+beangusd@users.noreply.github.com>
2023-03-18 18:47:32 -0400

    created thread with command, and got thread id

--- discordbot.py
+++ discordbot.py
@@ -7,8 +7,8 @@ load_dotenv(find_dotenv())
 TOKEN = os.getenv("TOKEN")
 
 
-bot = discord.Bot()
-greetings = bot.create_group("greetings", "Greet people")
+bot = discord.Bot(intents=discord.Intents.all())
+starting = bot.create_group("starting", "starts games")
 
 @bot.event
 async def on_ready():
@@ -16,13 +16,27 @@ async def on_ready():
 
 #create a command
 
-@greetings.command()
+@starting.command()
 async def start(ctx, user: discord.Member):
+  
+  #thread stuff
+
   thread = await ctx.channel.create_thread(name="ghost", auto_archive_duration=60, type=discord.ChannelType.private_thread)
+
+  global thread_id
+  thread_id = thread.id
+
   await thread.add_user(ctx.author)
   await thread.add_user(user)
   await ctx.send(f"Thread '{thread.name}' created!")
 
   await thread.send(f"This is a private thread between {ctx.author.mention} and {user.mention}!")
+  #thread stuff
+
+@bot.event
+async def on_thread_create(thread):
+   print("thread was created")
 
 bot.run(TOKEN)
+
+#TODO read all threads under general then get the thread id's then do the thing
