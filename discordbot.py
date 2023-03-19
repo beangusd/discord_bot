f91fc498f1d44a9af8bc390ccd731f918d2fb274
Roger Yi <80113181+beangusd@users.noreply.github.com>
2023-03-19 13:57:09 -0400

    Added basic api querying

--- discordbot.py
+++ discordbot.py
@@ -1,12 +1,16 @@
 import discord
 import os
 from dotenv import load_dotenv, find_dotenv
+import requests
+import json 
 
+url = ""
+
+#Loading TOKEN
 load_dotenv(find_dotenv())
-
 TOKEN = os.getenv("TOKEN")
 
-
+#SETTING UP INTENTS
 bot = discord.Bot(intents=discord.Intents.all())
 starting = bot.create_group("starting", "starts games")
 
@@ -14,13 +18,9 @@ starting = bot.create_group("starting", "starts games")
 async def on_ready():
     print('Logged in as {0.user}!'.format(bot))
 
-#create a command
-
 @starting.command()
 async def start(ctx, user: discord.Member):
   
-  #thread stuff
-
   thread = await ctx.channel.create_thread(name="ghost", auto_archive_duration=60, type=discord.ChannelType.private_thread)
 
   global thread_id
@@ -31,12 +31,35 @@ async def start(ctx, user: discord.Member):
   await ctx.send(f"Thread '{thread.name}' created!")
 
   await thread.send(f"This is a private thread between {ctx.author.mention} and {user.mention}!")
-  #thread stuff
 
 @bot.event
 async def on_thread_create(thread):
    print("thread was created")
+   #print(thread.id)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return 
+
+    if isinstance(message.channel, discord.Thread) and message.channel.id == thread_id:
+        print(f'Message received in thread {thread_id}: {message.content}')
+    
+        url = f'https://api.datamuse.com/words?sp={message.content}*&max=1'
+        r = requests.get(url)
+        data = json.loads(r.text)
+
+        word_list = []
+
+        for word in data:
+            name = word['word']
+            word_list.append(name)
+
+        l = len(word_list)
+
+        for l in word_list:
+            print(l)
 
 bot.run(TOKEN)
 
-#TODO read all threads under general then get the thread id's then do the thing
+#TODO manage games & listen for the correct threads.
